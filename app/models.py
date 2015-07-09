43656f8cf1cb13970ba1a2d53473abdca989a8a1
Imanol Aranzadi <ima@Imanols-MacBook-Pro.local>
2015-07-08 17:53:36 -0700

    added a registration/login form

--- app/models.py
+++ app/models.py
@@ -36,7 +36,9 @@ class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
+    phone = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
+    name = db.Column(db.String(64), index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
 
