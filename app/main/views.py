3e4f20b5312ad20a8e3c546e6602c7b9f651262d
inaki <iiaranzadi@gmail.com>
2015-07-24 10:10:22 -0700

    added some list user feature for asigning permissions to users

--- app/main/views.py
+++ app/main/views.py
@@ -92,7 +92,7 @@ def edit_profile_admin(id):
         user.about_me = form.about_me.data
         db.session.add(user)
         flash('The profile has been updated.')
-        return redirect(url_for('.user', username=user.username))
+        return redirect(url_for('.list_users', username=user.username))
     form.email.data = user.email
     form.username.data = user.username
     form.confirmed.data = user.confirmed
