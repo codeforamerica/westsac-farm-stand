08cbdfcd8c87aa3f5bfc04245b83c7f9f9dd70c8
Imanol Aranzadi <ima@Imanols-MacBook-Pro.local>
2015-07-29 12:25:28 -0700

    delete username and name field in registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -29,7 +29,7 @@ def logout():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(email=form.email.data.lower(),
+        user = User(email=form.email.data,
                     password=form.password.data)
         db.session.add(user)
         db.session.commit()
