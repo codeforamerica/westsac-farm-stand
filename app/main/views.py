28c4717f226bfc5d6558f8cf65b288d29f7a179d
inaki <iiaranzadi@gmail.com>
2015-07-29 13:12:45 -0700

    fix every view function using username

--- app/main/views.py
+++ app/main/views.py
@@ -45,9 +45,9 @@ def foodsms():
     resp.message(message)
     return str(resp)
 
-@main.route('/user/<username>')
-def user(username):
-    user = User.query.filter_by(username=username).first()
+@main.route('/user/<int:id>')
+def user(id):
+    user = User.query.filter_by(id=current_user.id).first()
     if user is None:
         abort(404)
     return render_template('user.html', user=user)
@@ -64,7 +64,7 @@ def edit_profile():
         db.session.add(current_user)
         db.session.commit()
         flash('Your profile has been updated.')
-        return redirect(url_for('.user', username=current_user.username))
+        return redirect(url_for('.user', user_id=current_user.user_id))
     form.name.data = current_user.name
     form.location.data = current_user.location
     form.about_me.data = current_user.about_me
@@ -79,7 +79,6 @@ def edit_profile_admin(id):
     form = EditProfileAdminForm(user=user)
     if form.validate_on_submit():
         user.email = form.email.data
-        user.username = form.username.data
         user.confirmed = form.confirmed.data
         user.role = Role.query.get(form.role.data)
         user.name = form.name.data
@@ -88,9 +87,8 @@ def edit_profile_admin(id):
         db.session.add(user)
         db.session.commit()
         flash('The profile has been updated.')
-        return redirect(url_for('.list_users', username=user.username))
+        return redirect(url_for('.list_users', id=user.user_id))
     form.email.data = user.email
-    form.username.data = user.username
     form.confirmed.data = user.confirmed
     form.role.data = user.role_id
     form.name.data = user.name
