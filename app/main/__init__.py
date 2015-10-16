36b031906c260148f619c2f6b595e14fe4343a2d
Grant Smith <grantrobertsmith@gmail.com>
2015-10-16 11:55:30 -0700

    add example api and marshmallow

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 main = Blueprint('main', __name__)
 
-from . import views, errors
+from . import views, errors, api
 from ..models import Permission
 
 @main.app_context_processor
